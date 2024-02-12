2b2702c23e7b500cd7b62d76296468ec4fc7548e
Joshua Gem Vicente <gemvicente6@gmail.com>
2024-02-12 12:43:11 +0800

    adada

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <div>Sidebar</div>
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,35 @@
-import React from "react";
 import axios from "axios";
+import { useState, React } from "react";
 import { Input } from "../components/ui/input";
 import { Wand } from "lucide-react";
 import { Button } from "../components/ui/button";
 import Navbar from "../components/Navbar";
 const Login = () => {
-  // const handleLogin = async () => {
-  //   const response = await axios.post('/api/auth/login', )
-  // }
-  //
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  // const history = useHistory();
+  const handleLogin = async () => {
+    if (!emailAddress || !password) {
+      setError("Please enter both email and password.");
+      return;
+    }
+    try {
+      const response = await axios.post("/api/auth/login", {
+        emailAddress,
+        password,
+      });
+
+      const { role } = response.data;
+
+      if (role === "dean") {
+        history.push("/dean/dashboard");
+      }
+    } catch (error) {
+      setError("Invalid email address or password. Please try again later.");
+      console.error(error);
+    }
+  };
 
   return (
     <div className="">
@@ -33,17 +54,23 @@ const Login = () => {
                 type="email"
                 className="w-50 px-5 my-10"
                 placeholder="Email"
+                value={emailAddress}
+                onChange={(e) => setEmailAddress(e.target.value)}
               />
               <Input
                 type="password"
                 className="w-50 px-5"
                 placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
-              <Button className="my-10">Login</Button>
+              <Button className="my-10" onClick={handleLogin}>
+                Login
+              </Button>
             </div>
           </div>
         </div>
-        <div className="w-3/4 bg-black relative h-full ">
+        <div className="w-3/4 bg-black  h-full ">
           {/* <img */}
           {/*   className="absolute inset-0 w-full h-svh" */}
           {/*   src="https://wallpapercrafter.com/desktop3/982680-communication-desk-electronics-home-office-indoors.jpg" */}
