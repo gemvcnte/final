8b0809e76ce8c871e1abdfcc75343677a7ccb7e3
Joshua Gem Vicente <gemvicente6@gmail.com>
2024-02-23 19:38:55 +0800

    abububwebwe

---
+++ src/pages/Faculty/Announcements.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FacultyAnnouncements = () => {
+  return (
+    <div>
+      <div>Announcements</div>
+    </div>
+  );
+};
+
+export default FacultyAnnouncements;
---
+++ src/pages/Faculty/Dashboard.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "@/components/Sidebar";
+import React from "react";
+
+const FacultyDashboard = () => {
+  return (
+    <div>
+      <Sidebar />
+      <div>Dashboard</div>
+    </div>
+  );
+};
+
+export default FacultyDashboard;
